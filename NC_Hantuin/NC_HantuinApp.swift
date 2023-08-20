c4d7cd51af4c39ead8560cd4f9e5a4ddd1b1d541
irvan <pradaindo24@gmail.com>
2023-08-20 23:25:42 +0700

    Initial Commit

---
+++ NC_Hantuin/NC_HantuinApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC_HantuinApp.swift
+//  NC_Hantuin
+//
+//  Created by Irvan P. Saragi on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NC_HantuinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
